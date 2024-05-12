c5adbb0e3ed8da260ee7e3a372aea1f5906eccec
Federico Maria Morrone <contact@morrone.dev>
2024-05-12 16:06:53 +0200

    Add description for status codes

--- effie/src/status.rs
+++ effie/src/status.rs
@@ -1,3 +1,5 @@
+use effie_macros::w;
+
 pub type Result<T = (), E = Status> = core::result::Result<T, E>;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -65,6 +67,53 @@ impl Status {
         match self {
             Self::SUCCESS => Ok(other),
             status => Err(status),
+        }
+    }
+
+    pub const fn description(&self) -> &[u16] {
+        match *self {
+            Self::SUCCESS => w!("The operation completed successfully."),
+            Self::LOAD_ERROR => w!("The image failed to load."),
+            Self::INVALID_PARAMETER => w!("A parameter was incorrect."),
+            Self::UNSUPPORTED => w!("The operation is not supported."),
+            Self::BAD_BUFFER_SIZE => w!("The buffer was not the proper size for the request."),
+            Self::BUFFER_TOO_SMALL => w!("The buffer is not large enough to hold the requested data."),
+            Self::NOT_READY => w!("There is no data pending upon return."),
+            Self::DEVICE_ERROR => w!("The physical device reported an error while attempting the operation."),
+            Self::WRITE_PROTECTED => w!("The device cannot be written to."),
+            Self::OUT_OF_RESOURCES => w!("A resource has run out."),
+            Self::VOLUME_CORRUPTED => w!("An inconstancy was detected on the file system causing the operating to fail."),
+            Self::VOLUME_FULL => w!("There is no more space on the file system."),
+            Self::NO_MEDIA => w!("The device does not contain any medium to perform the operation."),
+            Self::MEDIA_CHANGED => w!("The medium in the device has changed since the last access."),
+            Self::NOT_FOUND => w!("The item was not found."),
+            Self::ACCESS_DENIED => w!("Access was denied."),
+            Self::NO_RESPONSE => w!("The server was not found or did not respond to the request."),
+            Self::NO_MAPPING => w!("A mapping to a device does not exist."),
+            Self::TIMEOUT => w!("The timeout time expired."),
+            Self::NOT_STARTED => w!("The protocol has not been started."),
+            Self::ALREADY_STARTED => w!("The protocol has already been started."),
+            Self::ABORTED => w!("The operation was aborted."),
+            Self::ICMP_ERROR => w!("An ICMP error occurred during the network operation."),
+            Self::TFTP_ERROR => w!("A TFTP error occurred during the network operation."),
+            Self::PROTOCOL_ERROR => w!("A protocol error occurred during the network operation."),
+            Self::INCOMPATIBLE_VERSION => w!("The function encountered an internal version that was incompatible with a version requested by the caller."),
+            Self::SECURITY_VIOLATION => w!("The function was not performed due to a security violation."),
+            Self::CRC_ERROR => w!("A CRC error was detected."),
+            Self::END_OF_MEDIA => w!("Beginning or end of media was reached."),
+            Self::END_OF_FILE => w!("The end of the file was reached."),
+            Self::INVALID_LANGUAGE => w!("The language specified was invalid."),
+            Self::COMPROMISED_DATA => w!("The security status of the data is unknown or compromised and the data must be updated or replaced to restore a valid security status."),
+            Self::IP_ADDRESS_CONFLICT => w!("There is an address conflict address allocation"),
+            Self::HTTP_ERROR => w!("A HTTP error occurred during the network operation."),
+            Self::WARN_UNKNOWN_GLYPH => w!("The string contained one or more characters that the device could not render and were skipped."),
+            Self::WARN_DELETE_FAILURE => w!("The handle was closed, but the file was not deleted."),
+            Self::WARN_WRITE_FAILURE => w!("The handle was closed, but the data to the file was not flushed properly."),
+            Self::BUFFER_TOO_SMALL => w!("The resulting buffer was too small, and the data was truncated to the buffer size."),
+            Self::WARN_STALE_DATA => w!("The data has not been updated within the timeframe set by local policy for this type of data."),
+            Self::WARN_FILE_SYSTEM => w!("The resulting buffer contains UEFI-compliant file system."),
+            Self::WARN_RESET_REQUIRED => w!("The operation will be processed across a system reset."),
+            _ => w!("Unknown status.")
         }
     }
 }
